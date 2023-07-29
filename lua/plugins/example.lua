a515cb8171954fe4107dcafe095a4de8fe540372
CYFacu <cayofacundo1@gmail.com>
2023-07-29 11:18:28 -0300

    Added the transparency plugin

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -1,6 +1,5 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
-if true then return {} end
 
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
 --
@@ -9,6 +8,8 @@ if true then return {} end
 -- * disable/enabled LazyVim plugins
 -- * override the configuration of LazyVim plugins
 return {
+  { "xiyaowong/transparent.nvim" },
+  { "mhartington/formatter.nvim" },
   -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
 
